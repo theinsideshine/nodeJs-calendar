64e0744d9e2c84578e59b8d5657b1e907143ca00
theinsideshine <educacion.ta@gmail.com>
2023-05-14 22:18:06 -0300

    crud events

---
+++ routes/events.js
@@ -0,0 +1,45 @@
+/*
+    Event Routes
+    /api/events
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+
+
+const router = Router();
+
+// Todas tienes que pasar por la validación del JWT
+router.use( validarJWT );
+
+router.get('/',  getEventos);
+
+router.post('/',
+            [
+                check('title','El titulo es obligatorio').not().isEmpty(),
+                check('start','Fecha de inicio es obligatoria').custom( isDate ),
+                check('end','Fecha de finalización es obligatoria').custom( isDate ),
+                validarCampos
+            ],
+            crearEvento);
+
+router.put('/:id', 
+            [
+                check('title','El titulo es obligatorio').not().isEmpty(),
+                check('start','Fecha de inicio es obligatoria').custom( isDate ),
+                check('end','Fecha de finalización es obligatoria').custom( isDate ),
+                validarCampos
+            ],
+            actualizarEvento);
+
+router.delete('/:id', eliminarEvento);
+
+
+
+module.exports = router;
